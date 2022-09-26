346714e4f0fd9b4481ee21d8e797936ce1842974
rescate rescate <rescate@labinfo.is.escuelaing.edu.co>
2022-09-26 16:58:03 -0500

    no consulta

--- src/main/java/edu/eci/arsw/blueprints/controller/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controller/BlueprintAPIController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.blueprints.controllers;
+package edu.eci.arsw.blueprints.controller;
 
 import java.util.*;
 
